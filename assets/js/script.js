def68529c3ed0992043272039e9a6092cdc5234f
NB1355 <nahal5513@gmail.com>
2022-12-13 15:44:29 +0000

    till game type!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,56 @@
 document.addEventListener("DOMContentLoaded", function() {
+    
+
+    
     let buttons = document.getElementsByTagName("button");
+    
 
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
                 alert("You clicked Submit!");
+
             
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked ${gameType} !`);
+                runGame(gameType);
+                console.log(gameType);
             }
         });
     }
+    
 });
 
 
 
 
-function runGame() {
+function runGame(gameType) {
+
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+    
+    document.getElementById('operand1').textContent = num1;
+    document.getElementById('operand2').textContent = num2;  
+
+    switch(gameType) {
+        default:
+            document.getElementById('operator').textContent = "?";
+        
+        case "addition":
+            document.getElementById('operator').textContent = "+";
+          break;
+        case "subtract":
+            document.getElementById('operator').textContent = "-";
+          break;
+        case "multiply":
+            document.getElementById('operator').textContent = "*";
+          break;
+        case "division":
+            document.getElementById('operator').textContent = "/";
+          break;
+        
+      } 
 
 }
 
@@ -26,8 +59,19 @@ function checkAnswer() {
 }
 
 function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
 
 }
+
 
 function incrementScore() {
 
@@ -37,14 +81,3 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
-
-}
-
-function displaySubtractQuestion() {
-
-}
-
-function displayMultiplyQuestion() {
-    
-}
